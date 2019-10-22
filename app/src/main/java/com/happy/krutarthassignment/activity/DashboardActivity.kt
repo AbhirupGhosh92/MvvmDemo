f1e0a2f29ff40ffab9f18c6ec4ddf20568958705
Nawamita <namitapalande93@gmail.com>
2019-10-22 16:15:43 +0530

    gradle commit

--- app/src/main/java/com/happy/krutarthassignment/activity/DashboardActivity.kt
+++ app/src/main/java/com/happy/krutarthassignment/activity/DashboardActivity.kt
@@ -1,9 +1,10 @@
-package com.happy.krutarthassignment
+package com.happy.krutarthassignment.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.happy.krutarthassignment.R
 
-class MainActivity : AppCompatActivity() {
+class DashboardActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
